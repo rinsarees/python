50852dec13fcacd81128ab1fc6d89cce7fa1d30f
rinsarees <97509295+rinsarees@users.noreply.github.com>
2022-03-21 18:06:07 +0530

    Update list.py

--- list.py
+++ list.py
@@ -1,12 +1,4 @@
-list=[1,1,2,3,5,8,13,21,34,55,89]
- 
-new_list= [ ]
-  
-print(list)
-
-for element in list :
-
-    if element < 5 :
-        new_list .append(element)
-
-print("The list of numbers that are less than 5:", new_list)
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+for i in a:
+	if i < 5:
+		print(i)

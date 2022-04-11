a35dbfa2e2f27ae8484802e40b4e97b8291cc021
rinsarees <97509295+rinsarees@users.noreply.github.com>
2022-04-11 19:40:11 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,7 +1,4 @@
-n = int(input("\n\t\tEnter Lines To Read : "))
-f = open("sample.txt","r")
-for i in range(n):
-	print(f.readline())
+fileObject = open("sample.txt", "r")
+data = fileObject.read()
+print(data)
 
-
-	

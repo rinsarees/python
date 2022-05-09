9ec9a8b686c47b9697b59433fb384b0949c4e7f3
rinsarees <97509295+rinsarees@users.noreply.github.com>
2022-05-09 23:11:04 +0530

    Add files via upload

---
+++ sumlist.py
@@ -0,0 +1,3 @@
+list = [12, 8, 10, 2, 15]
+print(list)
+print("sum of list: ",sum(list))
---
+++ timestamp.py
@@ -0,0 +1,4 @@
+
+import datetime
+ 
+print(datetime.datetime.fromtimestamp(int("1294113662")) .strftime('%Y-%m-%d %H:%M:%S'))
